68588f13461f17e07bd514a429d1dcc411191b9c
jvenegas1 <j.venegasgutierrez@gmail.com>
2021-08-20 20:28:39 -0400

    interfaz dashboard

---
+++ ui.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(leaflet)
+library(plotly)
+library(DT)
+library(shinyjqui)
+library(polished)
+library(knitr)
+library(fresh)
+library(bs4Dash)
+library(waiter)
+library(xts)
+library(argonR)
+library(argonDash)
+library(shinyWidgets)
+library(shinyEffects)
+
+
+ui <- fluidPage(
+  useArgonDash(),setBackgroundColor(color = "ghostwhite"),
+  
+  
+  bs4DashPage(fullscreen = TRUE,dark = NULL,skin = "green",
+              dashboardHeader(title=bs4DashBrand(
+                title = "AgroSpace",
+                color = "primary",
+                href = "https://dash.agrospace.cl",
+                image = "https://pbs.twimg.com/profile_images/1063094754228011008/-AN6PKnB_400x400.jpg",opacity = 1
+                
+              )),
+              
+              
+              bs4DashSidebar(width = "1000",
+                             bs4SidebarMenu(
+                               #menuItem("Imagenes historicas",tabName = "modulo1",icon = icon("globe-americas")),
+                               bs4SidebarMenuItem("Imagenes Satelitales",tabName = "modulo1",icon = icon("globe-americas"))
+                             )
+              ),
+              bs4DashBody(bs4TabItems(
+                bs4TabItem(tabName = "modulo1",
+                           
+                           fluidRow(argonCard(title = "Potreros 2019",leafletOutput("plot1_19"),status = "primary"
+                                              ,width =4,maximizable = TRUE,floating  = TRUE,shadow = TRUE,gradient = TRUE,
+                                              background_color = "lightgreen",hover_shadow = TRUE),
+                                    argonCard(title = "Potreros 2020",leafletOutput("plot1_20") ,status = "primary"
+                                              ,width = 4,maximizable = TRUE,hover_lift = TRUE,shadow = TRUE,gradient = TRUE,
+                                              background_color = "lightgreen",hover_shadow = TRUE),
+                                    argonCard(title = "Potreros 2021",leafletOutput("plot1_21") ,status = "primary"
+                                              ,width = 4,maximizable = TRUE,hover_lift = TRUE,shadow = TRUE,gradient = TRUE,
+                                              background_color = "lightgreen",hover_shadow = TRUE))
+                           
+                           
+                )
+              )))
+)
+
+
+
